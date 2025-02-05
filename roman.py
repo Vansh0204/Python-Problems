24943fcbf293c5e9da8e67e9d469e624395b9103
Vansh Agarwal <vanshyt425@gmail.com>
2025-02-05 10:05:00 +0530

    Update roman.py

--- roman.py
+++ roman.py
@@ -41,3 +41,24 @@ Constraints:
 1 <= s.length <= 15
 s contains only the characters ('I', 'V', 'X', 'L', 'C', 'D', 'M').
 It is guaranteed that s is a valid roman numeral in the range [1, 3999].'''
+
+def romanToInt(s):
+        roman_values = {
+            'I': 1, 'V': 5, 'X': 10, 'L': 50,
+            'C': 100, 'D': 500, 'M': 1000
+        }
+        
+        total = 0
+        prev_value = 0
+
+        for char in reversed(s):
+            current_value = roman_values[char]
+            
+            if current_value < prev_value:
+                total -= current_value
+            else:
+                total += current_value
+            
+            prev_value = current_value
+
+        return total
