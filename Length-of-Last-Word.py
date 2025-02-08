3781aa498775eb8ef3ff31cc40da04446cb2f73e
Vansh Agarwal <vanshyt425@gmail.com>
2025-02-08 20:15:35 +0530

    Update Length-of-Last-Word.py

--- Length-of-Last-Word.py
+++ Length-of-Last-Word.py
@@ -12,3 +12,17 @@ Input: s = "Hello World"
 Output: 5
 Explanation: The last word is "World" with length 5.
 '''
+
+def lengthOfLastWord(s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        l=0
+        i=len(s)-1
+        while i >= 0 and s[i] == ' ':
+            i -=1
+        while i >=0 and s[i] != " ":
+            l+=1
+            i-=1
+        return l
