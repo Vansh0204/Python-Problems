4b5c8865528134ad668aeba4142b20eaccb0cf74
Vansh Agarwal <vanshyt425@gmail.com>
2025-02-09 20:01:37 +0530

    Update Integer_to_roman.py

--- Integer_to_roman.py
+++ Integer_to_roman.py
@@ -30,3 +30,20 @@ Explanation:
   40 = XL as 10 (X) less of 50 (L)
    9 = IX as 1 (I) less of 10 (X)
 Note: 49 is not 1 (I) less of 50 (L) because the conversion is based on decimal places'''
+
+def intToRoman(num):
+        roman_map = [
+            (1000, "M"), (900, "CM"), (500, "D"), (400, "CD"),
+            (100, "C"), (90, "XC"), (50, "L"), (40, "XL"),
+            (10, "X"), (9, "IX"), (5, "V"), (4, "IV"),
+            (1, "I")
+        ]
+        
+        result = []
+        
+        for value, symbol in roman_map:
+            while num >= value:
+                result.append(symbol)
+                num -= value
+        
+        return "".join(result)
