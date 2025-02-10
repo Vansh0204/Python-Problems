521e0128ed1a9e9125303db1dce6727b8e8ea5cb
Vansh Agarwal <vanshyt425@gmail.com>
2025-02-10 20:29:50 +0530

    Update LongestCommonPrefix.py

--- LongestCommonPrefix.py
+++ LongestCommonPrefix.py
@@ -13,3 +13,13 @@ Example 2:
 Input: strs = ["dog","racecar","car"]
 Output: ""
 Explanation: There is no common prefix among the input strings.'''
+
+def longestCommonPrefix(strs):
+        if len(strs)==0:
+            return " "
+        base = strs[0]
+        for i in range(len(base)):
+            for word in strs[1:]:
+                if(i==len(word)or word[i]!=base[i]):
+                    return base[0:i]
+        return base
