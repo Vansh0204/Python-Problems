da3cc3118cecdbac80f16342e5f38afaabadcfac
Vansh Agarwal <vanshyt425@gmail.com>
2025-02-11 23:52:47 +0530

    Update GenerateParentheses.py

--- GenerateParentheses.py
+++ GenerateParentheses.py
@@ -1,3 +1,14 @@
+''' 
+
+Example 1:
+
+Input: n = 3
+Output: ["((()))","(()())","(())()","()(())","()()()"]
+Example 2:
+
+Input: n = 1
+Output: ["()"]'''
+
 def generateParenthesis(self, n: int) -> List[str]:
         
         def dfs(left, right, s):
