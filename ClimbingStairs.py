0e482b17c07b056d21a461c102e4c85b087adf3f
Vansh Agarwal <vanshyt425@gmail.com>
2025-03-01 22:38:26 +0530

    Update ClimbingStairs.py

--- ClimbingStairs.py
+++ ClimbingStairs.py
@@ -20,3 +20,16 @@ Explanation: There are three ways to climb to the top.
 2. 1 step + 2 steps
 3. 2 steps + 1 step
 '''
+def climbStairs( n):
+     if n == 1:
+         return 1
+     if n == 2:
+         return 2
+
+     dp = [0] * (n + 1)
+     dp[1], dp[2] = 1, 2
+
+     for i in range(3, n + 1):
+         dp[i] = dp[i - 1] + dp[i - 2]
+
+     return dp[n]
