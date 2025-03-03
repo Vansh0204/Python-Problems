e979ff4276061d8bdc8b25a8e4e791cb4e821922
Vansh Agarwal <vanshyt425@gmail.com>
2025-03-03 19:35:37 +0530

    Create ImplementingInsertionsort.py

---
+++ ImplementingInsertionsort.py
@@ -0,0 +1,30 @@
+'''Given a sequence of size N, containing positive integers. You need to sort the elements of sequence using Insertion sort algorithm.
+Input
+User Task
+This is a function problem. You don't have to take any input. You are required to complete the function insertion_sort() that takes an array seq as parameter.
+
+Custom Input
+The first line of the input denotes the number of test cases 'T'.
+The first line of the test case is the size of the array.
+The second line of the test case consists of array elements.
+
+Constraints
+1 <= T <= 100
+1 <= N <= 10^3
+1 <= A[i] <= 10^3
+
+Sum of N over all test cases won't exceed 5 * 10^3
+Output
+Return Sorted list.
+Example
+Input:
+2
+5
+4 1 3 9 7
+10
+10 9 8 7 6 5 4 3 2 1
+
+Output:
+1 3 4 7 9
+1 2 3 4 5 6 7 8 9 10
+'''
