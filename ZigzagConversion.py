a9c1dda8977dcda35afc30dd933f5c9329a4b735
Vansh Agarwal <vanshyt425@gmail.com>
2025-03-07 23:51:20 +0530

    Update ZigzagConversion.py

--- ZigzagConversion.py
+++ ZigzagConversion.py
@@ -24,3 +24,21 @@ A   L S  I G
 Y A   H R
 P     I
 '''
+def convert(self, s, numRows):
+        if numRows == 1 or numRows >= len(s):
+            return s
+        
+        rows = ["" for _ in range(numRows)]
+        index, step = 0, 1
+        
+        for char in s:
+            rows[index] += char
+            if index == 0:
+                step = 1
+            elif index == numRows - 1:
+                step = -1
+            index += step
+        
+        return "".join(rows)
+
+        
