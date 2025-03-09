1c91689229e05ea06c0a5f221aa8335394655a4e
Vansh Agarwal <vanshyt425@gmail.com>
2025-03-09 23:57:18 +0530

    Create GenerateBinaryStrings.py

---
+++ GenerateBinaryStrings.py
@@ -0,0 +1,31 @@
+'''Given a positive integer 
+N
+N. The task is to print all the binary strings of length 
+N
+N in ascending order.
+
+Note: A binary string contains only 0 and 1.
+Input
+The first and only line of input contains a single integer 
+N
+N, representing the length of the binary string.
+
+Constraints
+1 <= N <= 15
+Output
+Print all the binary strings of length 
+N
+N in ascending order.
+Example
+Input
+3
+Output
+000
+001
+010
+011
+100
+101
+110
+111
+'''
