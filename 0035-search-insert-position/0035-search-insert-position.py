409c8eb5582d37f154bf3d9a5e3a79fb62af9eaf
Vansh Agarwal <anshag04j@gmail.com>
2026-02-23 11:44:16 +0530

    Time: 0 ms (100%), Space: 13 MB (32.49%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.py
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -5,12 +5,16 @@ class Solution(object):
         :type target: int
         :rtype: int
         """
-        a = len(nums)  # default insert at the end
-        for i in range(len(nums)):
-            if nums[i] == target:
-                a = i
-                break
-            elif nums[i] > target:
-                a = i
-                break
-        return a
+        left, right = 0, len(nums) - 1
+        
+        while left <= right:
+            mid = (left + right) // 2
+            
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+        
+        return left
