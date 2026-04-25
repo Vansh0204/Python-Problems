a144b20cb5d45c7cd408f2d094b0782fd5c312ad
Vansh Agarwal <anshag04j@gmail.com>
2026-04-25 23:08:08 +0530

    Time: 0 ms (100%), Space: 12.3 MB (92.19%) - LeetHub

---
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
@@ -0,0 +1,35 @@
+class Solution(object):
+    def myAtoi(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        i = 0
+        n = len(s)
+        INT_MIN = -2**31
+        INT_MAX = 2**31 - 1
+        
+        # Step 1: Skip leading spaces
+        while i < n and s[i] == " ":
+            i += 1
+        
+        # Step 2: Check sign
+        sign = 1
+        if i < n and (s[i] == "+" or s[i] == "-"):
+            if s[i] == "-":
+                sign = -1
+            i += 1
+        
+        # Step 3: Convert digits
+        num = 0
+        while i < n and s[i].isdigit():
+            digit = int(s[i])
+            
+            # Overflow check
+            if num > (INT_MAX - digit) // 10:
+                return INT_MAX if sign == 1 else INT_MIN
+            
+            num = num * 10 + digit
+            i += 1
+        
+        return sign * num
